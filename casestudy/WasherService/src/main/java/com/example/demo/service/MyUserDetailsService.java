7b4a7315cd089eccfb5986f79c3698a51f67d120
PendyalaGowtham <97961293+PendyalaGowtham@users.noreply.github.com>
2022-05-21 21:30:37 +0530

    washer+zuul

---
+++ casestudy/WasherService/src/main/java/com/example/demo/service/MyUserDetailsService.java
@@ -0,0 +1,35 @@
+package com.example.demo.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+import com.example.demo.model.WasherDetails;
+
+import com.example.demo.repo.WasherRepository;
+
+import java.util.ArrayList;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+	
+	@Autowired
+	private WasherRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
+ 
+        WasherDetails washer=repository.findByusername(name);
+       
+      return new User(washer.getUsername(),washer.getPassword(),new ArrayList<>());
+    	
+    }
+}
